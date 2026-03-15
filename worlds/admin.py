4638844ac2c7135c78655948452d8d5eda10b516
tobi-0909 <ishitobi0909@gmail.com>
2026-03-15 03:33:01 +0000

    feat: M2-04 Characterモデルとマイグレーションを追加
    closes #11
    
    - Character(world, name, profile, personality, created_at) を定義
    - Character を admin に登録
    - マイグレーション適用

--- worlds/admin.py
+++ worlds/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import World
+from .models import Character, World
 
 
 @admin.register(World)
@@ -8,3 +8,10 @@ class WorldAdmin(admin.ModelAdmin):
 	list_display = ('id', 'title', 'owner', 'created_at')
 	search_fields = ('title', 'owner__email', 'owner__handle')
 	list_filter = ('created_at',)
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'world', 'created_at')
+	search_fields = ('name', 'world__title', 'world__owner__email', 'world__owner__handle')
+	list_filter = ('created_at', 'world')
