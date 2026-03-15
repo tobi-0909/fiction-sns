95615f728e3894d8c1f428b1fecc19ffc6ef7345
tobi-0909 <ishitobi0909@gmail.com>
2026-03-15 01:45:47 +0000

    feat: M2-00 テンプレート基盤を整備
    closes #7
    
    - templates/base.html を追加
    - home/index をテンプレート返却へ変更
    - global templates/static ディレクトリ設定を追加
    - 最小のモバイル対応スタイルを追加

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Fictions flow SNS（仮）")
+    return render(request, "home/index.html")
